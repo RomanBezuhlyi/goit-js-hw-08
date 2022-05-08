636a42c35eb3820df967b027ba066c904f2e88da
Roman Bezuhlyi <bezuglijj7@gmail.com>
2022-05-08 18:52:40 +0300

    +gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector('.gallery');
+const galleryImage = createImgItem(galleryItems);
+galleryBox.insertAdjacentHTML("beforeend", galleryImage);
+
+
+function createImgItem(galleryItems) {
+   return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" 
+          alt="${description}">
+        </a>`;
+    })
+        .join('');
+}
+
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+})
